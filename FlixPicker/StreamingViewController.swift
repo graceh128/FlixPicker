4021744cc43d636dbaad63a1f83e8ef3ff340d4a
Grace Huang <grace_j_huang@outlook.com>
2022-06-17 05:40:04 -0400

    streaming sites

--- FlixPicker/StreamingViewController.swift
+++ FlixPicker/StreamingViewController.swift
@@ -6,13 +6,38 @@
 //
 
 import UIKit
-
+import WebKit
 class StreamingViewController: UIViewController {
-
+    @IBOutlet weak var netflixView: WKWebView!
+    @IBOutlet weak var disneyView: WKWebView!
+    @IBOutlet weak var huluView: WKWebView!
+    @IBOutlet weak var youtubeView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let googleURLA = URL(string: "https://www.google.com")
+        netflixView.load(URLRequest(url:googleURLA!))
+        disneyView.load(URLRequest(url:googleURLA!))
+        huluView.load(URLRequest(url:googleURLA!))
+        youtubeView.load(URLRequest(url:googleURLA!))
+    }
+    @IBAction func netflixButtonTapped(_ sender: Any) {
+        let googleURLA = URL(string: "netflix.com")
+        UIApplication.shared.open(googleURLA!)
+    }
+    @IBAction func disneyButtonTapped(_ sender: Any) {
+        let googleURLA = URL(string: "disneyplus.com")
+        UIApplication.shared.open(googleURLA!)
+    }
+    @IBAction func huluButtonTapped(_ sender: Any) {
+        let googleURLA = URL(string: "hulu.com")
+        UIApplication.shared.open(googleURLA!)
+    }
+    @IBAction func youtubeButtonTapped(_ sender: Any) {
+        let googleURLA = URL(string: "youtube.com")
+        UIApplication.shared.open(googleURLA!)
     }
     
 
